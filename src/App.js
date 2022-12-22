fdca8939221e46161e192a1d88156212efcb601d
aisaasghari <aisaasghari@gmail.com>
2022-12-23 01:25:47 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var imageURL = fetch("https://random-d.uk/api/random")
+    .then((Response) => Response.json())
+    .then((data) => {
+      return data.url;
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={imageURL} />
     </div>
   );
 }
