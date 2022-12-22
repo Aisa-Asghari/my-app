5ab2914ed63c79bf074c0146962162a74cc3f9f4
aisaasghari <aisaasghari@gmail.com>
2022-12-23 02:57:00 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import { Container, Row, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  var imageURL = fetch("https://random-d.uk/api/random")
-    .then((Response) => Response.json())
-    .then((data) => {
-      return data.url;
-    });
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    var auth = {
+      method: "GET",
+    };
+    fetch("https://random-d.uk/api/random", auth)
+      .then((result) => result.json())
+      .then((data) => setResponse(data));
+  }, []);
 
   return (
     <div className="App">
-      <img src={imageURL} />
+      <Container className="d-flex justify-content-center">
+        <Row className="p-5 w-50 h-50">
+          <Card>
+            <Card.Img src={response.url} className="p-5 img-fluid" />
+            <Card.Body>
+              <Card.Title className="p-4 text-center">
+                {response.message}
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
     </div>
   );
 }
